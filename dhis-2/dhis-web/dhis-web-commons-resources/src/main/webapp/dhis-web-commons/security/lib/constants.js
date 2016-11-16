2954a434acbe2e9ff6305af510305a0f4c4567e3
Sourabh Bhardwaj <bsourabh191@gmail.com>
2016-11-16 16:35:31 +0530

    script controllers & Constants

---
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/security/lib/constants.js
@@ -0,0 +1,18 @@
+/**
+ * Created by HISP-WS11 on 11/9/2016.
+ */
+
+
+const program = "PIhrNhsvPEr";
+const programStage = "y4x5OJBCqwm";
+
+
+const Name = "wKH9m17bZGm";
+const Email = "i8o3yEeWbo7";
+const Phone = "d46Y916iC7L";
+const whoRecieved = "OoVqstxfMUH";
+const facilityName = "dz6dmV6esrK";
+const servicesAvailed = "fy65k7rynVs";
+const Ratingvalue = "VuiizmvMtDs";
+const CommentsValue="obKc3ufS2h6";
+
