7e8c8ca3e5a8e6009b6694fb7e5ac176637d07f8
Vicky Rohilla <rohillavicky.172@gmail.com>
2017-11-23 17:13:01 +0530

    #11211 - Updation in Design of Dashboard
    Updation of logos and design for dashboard.

---
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-suaahara2-dashboard/js/custom.js
@@ -0,0 +1,363 @@
+$('.link1').click(function () {
+    $('#contentDiv').css({
+        'display': 'block'
+    });
+    $('.container').css({
+        'display': 'none'
+    });
+    $('#back_button').css({
+        'display': 'block'
+    });
+    $('.IR1').css({
+        'display': 'none'
+    });
+    $('.IR11').css({
+        'display': 'none'
+    });
+    $('.IR2').css({
+        'display': 'none'
+    });
+    $('.IR22').css({
+        'display': 'none'
+    });
+    $('.IR3').css({
+        'display': 'none'
+    });
+    $('.IR4').css({
+        'display': 'none'
+    });
+
+});
+
+
+
+$('.IR_11').click(function () {
+    $('.IR11').css({
+        'display': 'flex'
+    });
+    $('#a11').css({
+        'display': 'block'
+    });
+    $('.Demographic').css({
+        'display': 'none'
+    });
+    $('.Activity').css({
+        'display': 'none'
+    });
+    $('#a1').css({
+        'display': 'none'
+    });
+    $('#h1').css({
+        'display': 'none'
+    });
+    $('#d1').css({
+        'display': 'none'
+    });
+    $('#c1').css({
+        'display': 'none'
+    });
+    $('.IR1').css({
+        'display': 'none'
+    });
+    $('.IR2').css({
+        'display': 'none'
+    });
+    $('.IR3').css({
+        'display': 'none'
+    });
+    $('.IR4').css({
+        'display': 'none'
+    });
+    $('.IR22').css({
+        'display': 'none'
+    });
+    $('#a22').css({
+        'display': 'none'
+    });
+});
+
+
+$('.IR_22').click(function () {
+    $('.IR22').css({
+        'display': 'flex'
+    });
+    $('#a22').css({
+        'display': 'block'
+    });
+    $('.Demographic').css({
+        'display': 'none'
+    });
+    $('.Activity').css({
+        'display': 'none'
+    });
+    $('#a1').css({
+        'display': 'none'
+    });
+    $('#h1').css({
+        'display': 'none'
+    });
+    $('#d1').css({
+        'display': 'none'
+    });
+    $('#c1').css({
+        'display': 'none'
+    });
+    $('.IR1').css({
+        'display': 'none'
+    });
+    $('.IR2').css({
+        'display': 'none'
+    });
+    $('.IR3').css({
+        'display': 'none'
+    });
+    $('.IR4').css({
+        'display': 'none'
+    });
+    $('.IR11').css({
+        'display': 'none'
+    });
+    $('#a11').css({
+        'display': 'none'
+    });
+});
+
+
+
+
+$('.IR_1').click(function () {
+    $('.IR1').css({
+        'display': 'flex'
+    });
+    $('.Demographic').css({
+        'display': 'none'
+    });
+    $('.Activity').css({
+        'display': 'none'
+    });
+    $('#a1').css({
+        'display': 'block'
+    });
+    $('#h1').css({
+        'display': 'none'
+    });
+    $('#d1').css({
+        'display': 'none'
+    });
+    $('#c1').css({
+        'display': 'none'
+    });
+    $('.IR2').css({
+        'display': 'none'
+    });
+    $('.IR3').css({
+        'display': 'none'
+    });
+    $('.IR4').css({
+        'display': 'none'
+    });
+    $('#a11').css({
+        'display': 'none'
+    });
+    $('.IR11').css({
+        'display': 'none'
+    });
+    $('.IR22').css({
+        'display': 'none'
+    });
+    $('#a22').css({
+        'display': 'none'
+    });
+});
+
+
+$('.IR_2').click(function () {
+    $('.IR2').css({
+        'display': 'flex'
+    });
+    $('.Demographic').css({
+        'display': 'none'
+    });
+    $('.Activity').css({
+        'display': 'none'
+    });
+    $('#a1').css({
+        'display': 'none'
+    });
+    $('#d1').css({
+        'display': 'block'
+    });
+    $('#h1').css({
+        'display': 'none'
+    });
+    $('#c1').css({
+        'display': 'none'
+    });
+    $('.IR1').css({
+        'display': 'none'
+    });
+    $('.IR3').css({
+        'display': 'none'
+    });
+    $('.IR4').css({
+        'display': 'none'
+    });
+    $('#a11').css({
+        'display': 'none'
+    });
+    $('.IR11').css({
+        'display': 'none'
+    });
+    $('.IR22').css({
+        'display': 'none'
+    });
+    $('#a22').css({
+        'display': 'none'
+    });
+});
+
+
+$('.IR_3').click(function () {
+    $('.IR3').css({
+        'display': 'flex'
+    });
+    $('.Demographic').css({
+        'display': 'none'
+    });
+    $('.Activity').css({
+        'display': 'none'
+    });
+    $('#a1').css({
+        'display': 'none'
+    });
+    $('#d1').css({
+        'display': 'none'
+    });
+    $('#h1').css({
+        'display': 'block'
+    });
+    $('#c1').css({
+        'display': 'none'
+    });
+    $('.IR2').css({
+        'display': 'none'
+    });
+    $('.IR1').css({
+        'display': 'none'
+    });
+    $('.IR4').css({
+        'display': 'none'
+    });
+    $('#a11').css({
+        'display': 'none'
+    });
+    $('.IR11').css({
+        'display': 'none'
+    });
+    $('.IR22').css({
+        'display': 'none'
+    });
+    $('#a22').css({
+        'display': 'none'
+    });
+});
+
+
+$('.IR_4').click(function () {
+    $('.IR4').css({
+        'display': 'flex'
+    });
+    $('.Demographic').css({
+        'display': 'none'
+    });
+    $('.Activity').css({
+        'display': 'none'
+    });
+    $('#a1').css({
+        'display': 'none'
+    });
+    $('#d1').css({
+        'display': 'none'
+    });
+    $('#h1').css({
+        'display': 'none'
+    });
+
+    $('#c1').css({
+        'display': 'block'
+    });
+    $('.IR2').css({
+        'display': 'none'
+    });
+    $('.IR3').css({
+        'display': 'none'
+    });
+    $('.IR1').css({
+        'display': 'none'
+    });
+    $('#a11').css({
+        'display': 'none'
+    });
+    $('.IR11').css({
+        'display': 'none'
+    });
+    $('.IR22').css({
+        'display': 'none'
+    });
+    $('#a22').css({
+        'display': 'none'
+    });
+});
+
+
+
+$('#back_button').click(function () {
+    $('#contentDiv').css({
+        'display': 'none'
+    });
+    $('.container').css({
+        'display': 'block'
+    });
+    $('#back_button').css({
+        'display': 'none'
+    });
+    $('.houseHold1').css({
+        'display': 'none'
+    });
+    $('.Activity').css({
+        'display': 'none'
+    });
+    $('.Demographic').css({
+        'display': 'none'
+    });
+    $('#a1').css({
+        'display': 'none'
+    });
+    $('#a11').css({
+        'display': 'none'
+    });
+    $('#a22').css({
+        'display': 'none'
+    });
+    $('#d1').css({
+        'display': 'none'
+    });
+    $('#h1').css({
+        'display': 'none'
+    });
+    $('#c1').css({
+        'display': 'none'
+    });
+    $('.IR1').css({
+        'display': 'none'
+    });
+    $('.IR2').css({
+        'display': 'none'
+    });
+    $('.IR3').css({
+        'display': 'none'
+    });
+    $('.IR4').css({
+        'display': 'none'
+    });
+});
