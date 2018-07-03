f24f993b99b3d06f44cdb074f59da652081ce871
Sourabh Bhardwaj <bsourabh191@gmail.com>
2018-07-03 15:36:26 +0530

    Dhis2.getTable function deprecated , cahnge to reportTable

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/security/showtable.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/security/showtable.js
@@ -1,44 +1,87 @@
+// var arr=[];
+// var peid,orgid,orgname;
+//
+//
+// function showtable(arr,peid,orgid,orgname){
+//   console.log("values: "+ peid+orgid+orgname);
+//   console.log("values arr "+ arr);
+//
+//   DHIS.getTable({
+//     "columns": [
+//       {
+//         "dimension": "dx",
+//         "items": arr
+//       }
+//     ],
+//     "rows": [
+//       {
+//         "dimension": "pe",
+//         "items": [
+//           {
+//             "id": peid
+//           }
+//         ]
+//       },
+//       {
+//         "dimension": "ou",
+//         "items": [
+//           {
+//             "id": orgid,
+//             "name": orgname
+//           }
+//         ]
+//       }
+//
+//     ],
+//     "filters": null,
+//     "showColTotals": false,
+//     "showRowTotals": false,
+//     "showColSubTotals": false,
+//     "showRowSubTotals": false,
+//     "url": "../..",
+//     "el": "table1"
+//   });
+// }
+
 var arr=[];
 var peid,orgid,orgname;
 
 
 function showtable(arr,peid,orgid,orgname){
-  console.log("values: "+ peid+orgid+orgname);
-  console.log("values arr "+ arr);
+var r2 = {
+el: "table1",
+columns: [
+{
+"dimension": "dx",
+"items": arr
+}
+],
+rows: [
+{
+"dimension": "pe",
+"items": [
+{
+"id": peid
+}
+]
+},
+{
+"dimension": "ou",
+"items": [
+{
+"id": orgid,
+"name": orgname
+}
+]
+}
 
-  DHIS.getTable({
-    "columns": [
-      {
-        "dimension": "dx",
-        "items": arr
-      }
-    ],
-    "rows": [
-      {
-        "dimension": "pe",
-        "items": [
-          {
-            "id": peid
-          }
-        ]
-      },
-      {
-        "dimension": "ou",
-        "items": [
-          {
-            "id": orgid,
-            "name": orgname
-          }
-        ]
-      }
+],
+filters: [
+null
+],
 
-    ],
-    "filters": null,
-    "showColTotals": false,
-    "showRowTotals": false,
-    "showColSubTotals": false,
-    "showRowSubTotals": false,
-    "url": "../..",
-    "el": "table1"
-  });
+// All following properties are optional
+title: "My custom title"
+};
+reportTablePlugin.load([r2]);
 }
