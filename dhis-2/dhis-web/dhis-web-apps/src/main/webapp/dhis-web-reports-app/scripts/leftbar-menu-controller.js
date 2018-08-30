4243d8e942f7a0c6dbde576775bf81d64f4d2386
sudiksha1989 <snagvanshi06@gmail.com>
2018-08-30 16:46:51 +0530

    yearly option added

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-reports-app/scripts/leftbar-menu-controller.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-reports-app/scripts/leftbar-menu-controller.js
@@ -61,12 +61,26 @@ reportsApp.controller('LeftBarMenuController',
                     $scope.currentUser = responseUser;
                     //$scope.currentUserName = responseUser.userCredentials.username;
                     $scope.currentUserName = responseUser.userCredentials.username;// for 2.20
+                    $scope.currentUserRoles = responseUser.userCredentials.userRoles;
+                    $scope.superUserAuthority = "";
+                    for(var i=0 ; i < $scope.currentUserRoles.length; i++){
+                        $scope.currentUserRoleAuthorities = responseUser.userCredentials.userRoles[i].authorities;
+                        for(var j = 0 ; j < $scope.currentUserRoleAuthorities.length; j++)
+                        {
+                            if(  $scope.currentUserRoleAuthorities[j] === "ALL")
+                            {
+                                //$scope.accessAuthority = true;
+                                $scope.superUserAuthority = "YES";
+                                break;
+                            }
+                        }
+                    }
                     console.log("Current User Uid  --" + $scope.currentUser.id + "  Current User Name  --" + $scope.currentUserName);
 
-                    if(  $scope.currentUserName === 'admin')
-                    {
-                        $scope.accessAuthority = true;
-                    }
+                     if(  $scope.currentUserName === 'admin' || $scope.superUserAuthority === "YES" )
+                     {
+                         $scope.accessAuthority = true;
+                     }
 
                     //console.log("accessAuthority --" + $scope.accessAuthority);
                 });
