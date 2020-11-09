0316451877270a20519590f4acee8b50d2dd4041
Sourabh Bhardwaj <bsourabh191@gmail.com>
2020-11-09 18:09:26 +0530

    captcha validations for recovery page

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/useraccount/recovery.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/useraccount/recovery.js
@@ -8,31 +8,64 @@ $(document).ready(function() {
   if( undefined !== locale && locale ) {
     login.changeLocale(locale);
   }
+  drawComplexCaptcha();
+  //Disable cut copy paste
+  $('body').bind('cut copy paste', function (e) {
+    e.preventDefault();
+  });
+
+  //Disable mouse right click
+  $("body").on("contextmenu",function(e){
+    return false;
+  });
 
 });
 
 function recoverAccount() {
+  //alert( "inside captch validation");
   var username = $.trim($("#username").val());
+  var enteredCaptcha = document.getElementById("txtCaptchaInput").value;
+  var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+  var str2 = removeSpaces(document.getElementById('txtCaptchaInput').value);
 
-  if( username.length == 0 ) {
+  if( enteredCaptcha == null || enteredCaptcha == "" || enteredCaptcha == " " )
+  {
+    alert("Please Enter Captcha");
+    //document.getElementById("submit").disabled = false;
     return false;
   }
-
-  $.ajax({
-    url: "../../api/account/recovery",
-    data: {
-      username: username
-    },
-    type: "post",
-    success: function(data) {
-      $("#recoveryForm").hide();
-      $("#recoverySuccessMessage").fadeIn();
-    },
-    error: function(data) {
-      $("#recoveryForm").hide();
-      $("#recoveryErrorMessage").fadeIn();
+  else if( str1 != str2 )
+  {
+    alert("Invalid Captcha");
+    document.getElementById("txtCaptchaInput").value = "";
+    //document.getElementById("submit").disabled = false;
+    location.reload();
+    return false;
+  }
+  else if( str1 == str2 )
+  {
+    if( username.length == 0 ) {
+      return false;
     }
-  });
+
+    $.ajax({
+      url: "../../api/account/recovery",
+      data: {
+        username: username
+      },
+      type: "post",
+      success: function(data) {
+        $("#recoveryForm").hide();
+        $("#recoverySuccessMessage").fadeIn();
+      },
+      error: function(data) {
+        $("#recoveryForm").hide();
+        $("#recoveryErrorMessage").fadeIn();
+      }
+    });
+    return true;
+  }
+
 }
 
 login.changeLocale = function(locale) {
@@ -44,3 +77,49 @@ login.changeLocale = function(locale) {
     $('#recoveryErrorMessage').html(json.recover_error_message);
   });
 };
+
+
+function drawCaptcha()
+{
+  var a = Math.ceil(Math.random() * 10)+ '';
+  var b = Math.ceil(Math.random() * 10)+ '';
+  var c = Math.ceil(Math.random() * 10)+ '';
+  var d = Math.ceil(Math.random() * 10)+ '';
+  var e = Math.ceil(Math.random() * 10)+ '';
+  var f = Math.ceil(Math.random() * 10)+ '';
+  var g = Math.ceil(Math.random() * 10)+ '';
+  var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' '+ f + ' ' + g;
+  document.getElementById("txtCaptcha").value = code;
+  document.getElementById("txtCaptchaInput").value = "";
+}
+
+function drawComplexCaptcha()
+{
+  var alpha = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+    '0','1','2','3','4','5','6','7','8','9',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+  var a = alpha[Math.floor(Math.random()*62)];
+  var b = alpha[Math.floor(Math.random()*62)];
+  var c = alpha[Math.floor(Math.random()*62)];
+  var d = alpha[Math.floor(Math.random()*62)];
+  var e = alpha[Math.floor(Math.random()*62)];
+  var f = alpha[Math.floor(Math.random()*62)];
+
+  var code = a + b  + c + d + e + f;
+  document.getElementById("txtCaptcha").value = code;
+  document.getElementById("txtCaptchaInput").value = "";
+}
+
+
+
+
+
+// Remove the spaces from the entered and generated code
+function removeSpaces(string)
+{
+  return string.split(' ').join('');
+}
+
+
